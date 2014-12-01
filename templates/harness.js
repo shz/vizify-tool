876b698822d3071197dc6ffbf5619b0ed6570f83
dfsklar <dfsklar@gmail.com>
2014-12-01 11:49:45 -0500

    Support for a replay button to be displayed if settings.replayable is true. The replay button is an unstyled orange box that is positioned currently below the canvas.  Patrick will style and position, since positioning is proving difficult due to (IMO) the table-cell-structured BODY. Currently, for development purposes, the settings structure is set up to start the movie late in the game (8000ms into the movie) so the end of the movie is reached rapidly.

--- templates/harness.js
+++ templates/harness.js
@@ -1,9 +1,16 @@
 // Dummy -- need to replace
 var querySt = function(name, def) { return def; };
 
+
 (function() {
   var canvas = window.canvas = new vizify.Canvas({{width}}, {{height}}, 1);
   document.body.appendChild(canvas.canvas);
+
+  // Patrick will edit this to obtain settings.
+  var settings = {
+    replayable: true,
+    startWithFrameNumber: 8300  // Useful during devel.  Default: null
+  };
 
   // Sizes the card based on current window size
   var size = function() {
@@ -19,6 +26,7 @@ var querySt = function(name, def) { return def; };
 
     canvas.resize({{width}}, {{height}}, scaleFactor);
   };
+
   size();
 
   // Trigger size() on debounced window size event
@@ -38,6 +46,32 @@ var querySt = function(name, def) { return def; };
       if (xhr.status == 200) {
         var card = window.card = new vizify.Card(canvas, {{entryPoint}}, xhr.responseText);
         card.load(function() {
+          if (settings.replayable) {
+            card.on('end', function() {
+              // Each time the end of the movie is reached, a new
+              // Replay button is constructed.  The button element
+              // is deleted if the user initiates a Replay, so
+              // there is no leak in the case of multiple replays.
+              console.log("Handling the movie END event...");
+              var elemCanv = card.canvas.canvas;
+              var btn = document.createElement('div');
+              btn.setAttribute('class', 'button-replay');
+              btn.appendChild(document.createTextNode("Replay"));
+              var btnParent = elemCanv.parentElement;
+              btnParent.appendChild(btn);
+              btn.addEventListener('click', function() {
+                // Handle user click on the Replay button.
+                console.log("Handling user click on REPLAY button...");
+                card.seek(0);
+                card.play();
+                btnParent.removeChild(btn);
+              });
+            });
+          }
+          if (settings.startWithFrameNumber) {
+            console.log("Jumping to frame " + settings.startWithFrameNumber + " before starting...");
+            card.seek(settings.startWithFrameNumber);
+          }
           card.play();
         });
       } else {
