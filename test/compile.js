f90d13e550ffb9539f959de3b9b104c9fd981cfb
Patrick Stein <lpstein@yahoo-inc.com>
2015-01-12 18:02:00 -0800

    More tests

--- test/compile.js
+++ test/compile.js
@@ -1,19 +1,36 @@
+var async = require('async');
 var compile = require('./util/require')('compile');
 
 var dir = require('path').join(__dirname, 'util', 'misc', 'harness_test');
+var langs = [];
+
+var eachLang = function(f, cb) {
+  async.forEach(['javascript', 'cpp'], f, cb);
+};
 
 exports.testValid = function(test, assert) {
-  compile.compileCard({dir: dir, name: 'foo-bar'}, 'javascript', function(err, body) {
-    assert.ifError(err);
-    assert.isDefined(body);
+  eachLang(function(language, callback) {
+    compile.compileCard({dir: dir, name: 'foo-bar'}, language, function(err, body) {
+      assert.ifError(err);
+      assert.isDefined(body);
 
+      callback();
+    });
+  }, function(err) {
+    assert.ifError(err);
     test.finish();
   });
 };
 
 exports.testMissing = function(test, assert) {
-  compile.compileCard({dir: dir + '/foo', name: 'foo-bar'}, 'javascript', function(err, body) {
-    assert.isDefined(err);
+  eachLang(function(language, callback) {
+    compile.compileCard({dir: dir + '/foo', name: 'foo-bar'}, language, function(err, body) {
+      assert.isDefined(err);
+
+      callback();
+    });
+  }, function(err) {
+    assert.ifError(err);
 
     test.finish();
   });
