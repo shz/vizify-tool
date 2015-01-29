2e895d607b522a640e2c1eff2f20da18cad2ff11
Jeff Cutler-Stamm <jeffcs@yahoo-inc.com>
2015-01-28 17:06:18 -0800

    Fixed failing test

--- test/parse-libs.js
+++ test/parse-libs.js
@@ -5,8 +5,8 @@ exports.testAll = function(test, assert) {
   // null input should result in a 1 element array with default lib path ending in 'tool/src'
   var libs = parseLibs();
   assert.equal(libs.length, 1);
-  assert.match(libs[0], /tool\/src$/);
-
+  assert.match(libs[0], /\/src$/);
+  
   // a single path should be first element of returned array w/ default path the second
   libs = parseLibs('foo/bar');
   assert.equal(libs.length, 2);
