4a9ef330601b078898bc90ba2244100d02ca9dcc
Jeff Cutler-Stamm <jeffcs@yahoo-inc.com>
2015-02-02 14:37:11 -0800

    Switched to node-uuid from homegrown

--- test/uuid.js
+++
@@ -1,7 +0,0 @@
-var generateUUID = require('./util/require')('util/uuid');
-
-exports.testAll = function(test, assert) {
-  var uuid = generateUUID();
-  assert.match(uuid, /[a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12}/);
-  test.finish();
-};
