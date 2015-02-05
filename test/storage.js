2e9cdc713a1850f4e5fc7d936a205a220e679bab
Jeff Cutler-Stamm <jeffcs@yahoo-inc.com>
2015-02-05 15:59:06 -0800

    added stubbed storage test

---
+++ test/storage.js
@@ -0,0 +1,11 @@
+var storage = require('./util/require')('util/storage');
+
+exports.testPut = function(test, assert) {
+  // todo:
+  test.finish();
+};
+
+exports.testCheckExisting = function(test, assert) {
+  // todo:
+  test.finish();
+};
