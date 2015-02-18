71e223822265beef87c32f4e92dc0b1cec27fec8
Cedric Han <cedrich@yahoo-inc.com>
2015-02-18 15:10:09 -0800

    Pass in `size` instead of `width` and `height`
    Bump version to 2.0.3

--- templates/harness.js
+++ templates/harness.js
@@ -3,23 +3,25 @@ var querySt = function(name, def) { return def; };
 
 
 (function() {
-  var canvas = window.canvas = new vizify.Canvas({{width}}, {{height}}, 1);
-  document.getElementById('holder').appendChild(canvas.canvas);
+  {{#size}}
+    var canvas = window.canvas = new vizify.Canvas({{width}}, {{height}}, 1);
+    document.getElementById('holder').appendChild(canvas.canvas);
 
-  // Sizes the card based on current window size
-  var size = function() {
-    var cardRatio = {{width}} / {{height}};
-    var screenRatio = window.innerWidth / window.innerHeight;
+    // Sizes the card based on current window size
+    var size = function() {
+      var cardRatio = {{width}} / {{height}};
+      var screenRatio = window.innerWidth / window.innerHeight;
 
-    var scaleFactor = 0;
-    if (cardRatio > screenRatio) {
-      scaleFactor = window.innerWidth / {{width}};
-    } else {
-      scaleFactor = window.innerHeight / {{height}};
-    }
+      var scaleFactor = 0;
+      if (cardRatio > screenRatio) {
+        scaleFactor = window.innerWidth / {{width}};
+      } else {
+        scaleFactor = window.innerHeight / {{height}};
+      }
 
-    canvas.resize({{width}}, {{height}}, scaleFactor);
-  };
+      canvas.resize({{width}}, {{height}}, scaleFactor);
+    };
+  {{/size}}
 
   size();
 
